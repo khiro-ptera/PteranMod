cbc773dbca3b844076523a08922b52b9bab14f5a
Steven R. Xie <48636116+khiro-ptera@users.noreply.github.com>
2023-12-22 18:37:50 -0800

    Dusts added

---
+++ Dusts/Catalyst.cs
@@ -0,0 +1,23 @@
+﻿
+using Terraria;
+using Terraria.ModLoader;
+
+namespace PteranMod.Dusts
+{
+	public class Catalyst : ModDust
+	{
+		public override void OnSpawn(Dust dust)
+		{
+			dust.velocity *= 0f;
+			dust.noGravity = true;
+			dust.noLight = true;
+			dust.scale *= 1f;
+		}
+
+		public override bool Update(Dust dust)
+		{
+			dust.rotation += 0.1f;
+			return false;
+		}
+	}
+}
---
+++ Dusts/Spark.cs
@@ -0,0 +1,32 @@
+﻿
+using Terraria;
+using Terraria.ModLoader;
+
+namespace PteranMod.Dusts
+{
+	public class Spark : ModDust
+	{
+		public override void OnSpawn(Dust dust)
+		{
+			dust.velocity *= 0.6f;
+			dust.noGravity = true;
+			dust.noLight = true;
+			dust.scale *= 1.5f;
+		}
+
+		public override bool Update(Dust dust)
+		{
+			dust.velocity *= 1.05f;
+			dust.position += dust.velocity;
+			dust.rotation += dust.velocity.X;
+			dust.scale *= 0.99f;
+			float light = 0.35f * dust.scale;
+			Lighting.AddLight(dust.position, light, light, light);
+			if (dust.scale < 0.5f)
+			{
+				dust.active = false;
+			}
+			return false;
+		}
+	}
+}
